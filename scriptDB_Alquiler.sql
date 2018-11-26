0ec6f7eca4b33d80d9070e2b52e918ff70c6b152
JosueCuba <elian.cuba@vallegrande.edu.pe>
2018-11-26 09:24:49 -0500

    He creado  la base de datos

--- scriptDB_Alquiler.sql
+++ scriptDB_Alquiler.sql
@@ -3,3 +3,13 @@ SHOW DATABASES;
 
 /* ELIMINAR LA BASE DE DATOS BDALQUILER*/
 DROP DATABASE dbalquiler;
+/*crear nuestra base de datos alquiler teniendo en cuenbtra la configuracion*/
+CREATE DATABASE dbAlquiler DEFAULT CHARACTER set utf8;
+
+/*Poner en uso la base de datos*/
+USE dbAlquiler;
+ /*Verificar la bas e dedatos este en uso*/
+ SELECT DATABASE ();
+
+ /**/
+
